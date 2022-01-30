6abee1ce0d9023e8fd80640a553b0034bbbc2855
skyclouds2001 <95597335+skyclouds2001@users.noreply.github.com>
2022-01-30 17:48:02 +0800

    docs 20210128

---
+++ 1.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+		map<int, int> m;
+		vector<int> v;
+			
+		for(int i = 0; i < nums.size(); ++i) {
+			map<int, int>::iterator it = m.find(target - nums[i]);
+			if(it != m.end()) {
+				v.push_back(it->second);
+				v.push_back(i);
+				break;
+			}
+			map.insert(pair<int, int>(nums[i], i));
+		}
+		
+		return v;
+    }
+};
+
+int main() {
+}
+ 
