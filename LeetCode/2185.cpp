7a177721d0fbe2c3c45098e6dcc071429d86c689
skyclouds2001 <skyclouds2001@163.com>
2023-01-08 14:10:50 +0800

    feat: done 2185

---
+++ LeetCode/2185.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int num = 0;
+        for(auto it : words) {
+            size_t t = it.find(pref);
+            if (t != string::npos && t == 0) ++num;
+        }
+        return num;
+    }
+};
