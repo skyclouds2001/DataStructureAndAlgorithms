c1436b62ee38f8abf51bbbd869328563d2b7aa96
skyclouds2001 <skyclouds2001@163.com>
2023-01-10 19:51:39 +0800

    feat: done 35

--- LeetCode/35.cpp
+++ LeetCode/35.cpp
@@ -1,41 +1,45 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int res = -1;
-
-        for (int l = 0, r = nums.size() - 1; l <= r; ) {
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        int ans = -1;
+        while (l <= r) {
             int mid = (l + r) / 2;
             if (nums[mid] > target) {
                 r = mid - 1;
             } else if (nums[mid] < target) {
                 l = mid + 1;
             } else {
-                res = mid;
+                ans = mid;
                 break;
             }
         }
-
-        return res;
+        if (ans == -1) {
+            ans = l;
+        }
+        return ans;
     }
 };
 
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int res = -1;
-
-        for (int l = 0, r = nums.size(); l < r; ) {
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0, r = nums.size();
+        int ans = -1;
+        while (l < r) {
             int mid = (l + r) / 2;
             if (nums[mid] > target) {
                 r = mid;
             } else if (nums[mid] < target) {
                 l = mid + 1;
             } else {
-                res = mid;
+                ans = mid;
                 break;
             }
         }
-
-        return res;
+        if (ans == -1) {
+            ans = l;
+        }
+        return ans;
     }
 };
