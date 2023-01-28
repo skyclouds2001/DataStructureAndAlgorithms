26b973f953e0d7dca4f85f773caafd29e6e48032
skyclouds2001 <skyclouds2001@163.com>
2023-01-29 01:18:59 +0800

    feat: small changes

--- LeetCode/26.cpp
+++ LeetCode/26.cpp
@@ -2,10 +2,13 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         int len = nums.size();
-        int fast = 1, slow = 1;
+        int fast = 1; // 当前遍历位置
+        int slow = 1; // 当前待赋值位置
         while (fast < len) {
             if (nums[fast] != nums[fast - 1]) {
-                nums[slow] = nums[fast];
+                if (slow != fast) {
+                    nums[slow] = nums[fast];
+                }
                 ++slow;
             }
             ++fast;
--- LeetCode/27.cpp
+++ LeetCode/27.cpp
@@ -2,10 +2,13 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         int len = nums.size();
-        int fast = 0, slow = 0;
+        int fast = 0; // 当前遍历位置
+        int slow = 0; // 当前待赋值位置
         while (fast < len) {
             if (nums[fast] != val) {
-                nums[slow] = nums[fast];
+                if (fast != slow) {
+                    nums[slow] = nums[fast];
+                }
                 ++slow;
             }
             ++fast;
