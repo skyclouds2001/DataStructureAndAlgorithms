706132ed3c8dbeaf4f511bd5cc5097586a9d235a
skyclouds2001 <skyclouds2001@163.com>
2023-04-30 00:33:30 +0800

    feat: done 2618

---
+++ LeetCode/2618.js
@@ -0,0 +1,12 @@
+/**
+ * @param {any} object
+ * @param {any} classFunction
+ * @return {boolean}
+ */
+var checkIfInstanceOf = function (obj, classFunction) {
+  return (![null, undefined].includes(obj) && ![null, undefined].includes(classFunction) && typeof classFunction === 'function' && Object(obj) instanceof classFunction);
+};
+
+/**
+* checkIfInstanceOf(new Date(), Date); // true
+*/
