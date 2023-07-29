a1fb9f956bd3944f50600b792ced888a232bb881
skyclouds2001 <skyclouds2001@163.com>
2023-07-29 23:33:31 +0800

    feat: done 876

---
+++ LeetCode/876.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+
+        while (fast != nullptr && fast -> next != nullptr) {
+            fast = fast -> next -> next;
+            slow = slow -> next;
+        }
+
+        return slow;
+    }
+};
