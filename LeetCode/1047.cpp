7b5a76e4000c5bc0be38b9e6ac5cd14937b641e9
skyclouds2001 <95597335+skyclouds2001@users.noreply.github.com>
2023-08-04 11:34:23 +0800

    Create 1047.cpp

---
+++ LeetCode/1047.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        std::stack<char> stack;
+
+        for (auto c: s) {
+            if (stack.empty() || stack.top() != c) {
+                stack.push(c);
+            } else {
+                stack.pop();
+            };
+        }
+
+        string res = "";
+        while (!stack.empty()) {
+            res.push_back(stack.top());
+            stack.pop();
+        }
+
+        std::reverse(res.begin(), res.end());
+        
+        return res;
+    }
+};
