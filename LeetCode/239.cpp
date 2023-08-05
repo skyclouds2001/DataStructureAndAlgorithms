7ae5e0e76265d0fcc9c4dc017af19ac65e7889bf
skyclouds2001 <skyclouds2001@163.com>
2023-08-06 00:03:27 +0800

    feat: done 239

---
+++ LeetCode/239.cpp
@@ -0,0 +1,41 @@
+#include "deque"
+#include "vector"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> que;
+
+        int size = nums.size();
+
+        vector<int> ans;
+
+        for (int i = 0; i < k; ++i) {
+            int val = nums.at(i);
+            while (!que.empty() && val > que.back()) {
+                que.pop_back();
+            }
+            que.push_back(val);
+        }
+
+        ans.push_back(que.front());
+
+        for (int i = k; i < size; ++i) {
+            if (!que.empty() && nums[i - k] == que.front()) {
+                que.pop_front();
+            }
+
+            int val = nums.at(i);
+            while (!que.empty() && val > que.back()) {
+                que.pop_back();
+            }
+            que.push_back(val);
+
+            ans.push_back(que.front());
+        }
+
+        return ans;
+    }
+};
