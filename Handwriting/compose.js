c162e19e825bdcbb2a96486cd9d9d4c4dd890438
skyclouds2001 <95597335+skyclouds2001@users.noreply.github.com>
2023-08-07 20:18:21 +0800

    Update compose.js

--- Handwriting/compose.js
+++ Handwriting/compose.js
@@ -7,5 +7,7 @@ function compose(...fns) {
     return (...args) => {
       return res(it(...args))
     }
-  }, (...args) => args)
+  }, (args) => args)
 }
+
+console.log(compose(v => v + 1, v => v + 2, v => v + 3, v => v + 4)(5))
