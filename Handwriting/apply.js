a457fd8ab6bbb75d9858ce99d2d843bc22d05a6a
skyclouds2001 <95597335+skyclouds2001@users.noreply.github.com>
2023-08-09 15:06:49 +0800

    Update apply.js

--- Handwriting/apply.js
+++ Handwriting/apply.js
@@ -1,10 +1,13 @@
+const assert = require("assert")
+const { describe, before, after, it } = require("node:test")
+
 /**
  * @param {any} thisArgs
  * @param {any[]} args
  * @returns {any}
  */
-function apply(thisArgs, args) {
-  if (!thisArgs) thisArgs = globalThis ?? window ?? global
+function apply(thisArgs, args = []) {
+  if (thisArgs == null) thisArgs = globalThis ?? window ?? global
   thisArgs = typeof thisArgs === 'object' && thisArgs !== null ? thisArgs : Object(thisArgs)
 
   const f = Symbol()
@@ -20,12 +23,60 @@ function apply(thisArgs, args) {
   return res
 }
 
-let fn = function (name) {
-  console.log(this, name)
-}
+/***** test *****/
 
-Function.prototype.myApply = apply
+describe('apply', () => {
+  const GLOBAL_NAME = 'global name'
+  const OBJECT_NAME = 'object name'
+  const f = function(name) {
+    return name
+  }
+  const fn = function () {
+    return this.name
+  }
 
-fn.myApply(null, ['test'])
-fn.myApply(true, ['test'])
-fn.myApply({ name: 'base' }, ['test'])
+  before(() => {
+    Function.prototype.myApply = apply
+    globalThis.name = GLOBAL_NAME
+  })
+
+  after(() => {
+    delete Function.prototype.myApply
+    delete globalThis.name
+  })
+
+  it('should apply with globalThis without params', () => {
+    const name = fn.myApply()
+    assert.strictEqual(name, GLOBAL_NAME)
+  })
+
+  it('should apply with given object param', () => {
+    const name = fn.myApply({ name: OBJECT_NAME })
+    assert.strictEqual(name, OBJECT_NAME)
+  })
+
+  it('should apply with wrap class with null or undefined value param', () => {
+    const name1 = fn.myApply(null)
+    assert.strictEqual(name1, GLOBAL_NAME)
+    const name2 = fn.myApply(undefined)
+    assert.strictEqual(name2, GLOBAL_NAME)
+  })
+
+  it('should apply with wrap class with primitive value param', () => {
+    const name1 = fn.myApply(1)
+    assert.strictEqual(name1, undefined)
+    const name2 = fn.myApply('str')
+    assert.strictEqual(name2, undefined)
+    const name3 = fn.myApply(true)
+    assert.strictEqual(name3, undefined)
+    const name4 = fn.myApply(Symbol('sym'))
+    assert.strictEqual(name4, undefined)
+    const name5 = fn.myApply(1345432n)
+    assert.strictEqual(name5, undefined)
+  })
+
+  it('should apply with function param', () => {
+    const name = f.myApply({}, [OBJECT_NAME])
+    assert.strictEqual(name, OBJECT_NAME)
+  })
+})
