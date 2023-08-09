4f96dfdf1dc4ff92e14159c4097b70f4b4328997
skyclouds2001 <95597335+skyclouds2001@users.noreply.github.com>
2023-08-09 17:41:50 +0800

    Update 2650.js

--- LeetCode/2650.js
+++ LeetCode/2650.js
@@ -41,3 +41,40 @@ var cancellable = function(generator) {
 
     return [cancel, promise]
 };
+
+/**
+ * @param {Generator} generator
+ * @return {[Function, Promise]}
+ */
+var cancellable = function(generator) {
+    let cancel
+
+    const promise = new Promise(async (resolve, reject) => {
+      cancel = () => {
+        run(undefined, 'Cancelled')
+      }
+
+      const run = (res, error) => {
+        try {
+          const { value, done } = error ? generator.throw(error) : generator.next(res)
+          if (done) {
+            resolve(value)
+            return
+          } else {
+            value.then((value) => {
+              run(value)
+            }).catch((error) => {
+              run(undefined, error)
+            })
+          }
+        } catch (error) {
+          reject(error)
+        }
+      }
+
+      run()
+    })
+
+    return [cancel, promise]
+};
+
