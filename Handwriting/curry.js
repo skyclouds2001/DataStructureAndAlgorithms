21abd6c0437989ae7deeeb5da26ca0ddbbe404bd
skyclouds2001 <95597335+skyclouds2001@users.noreply.github.com>
2023-08-10 11:03:28 +0800

    Create curry.js

---
+++ Handwriting/curry.js
@@ -0,0 +1,30 @@
+const assert = require("node:assert")
+const { describe, it } = require("node:test")
+
+/**
+ * @param {Function} func
+ * @param {...any} args
+ * @returns {Function}
+ */
+function curry(func, ...args) {
+  return function(...params) {
+    if (args.length + params.length < func.length) {
+      return curry(func, ...args, ...params)
+    } else {
+      return func.call(this, ...args, ...params)
+    }
+  }
+}
+
+/***** test *****/
+
+describe('curry', () => {
+  const sum = (num1, num2, num3) => num1 + num2 + num3
+
+  it('should curry function', () => {
+    assert.strictEqual(curry(sum)(1, 2, 3), 6)
+    assert.strictEqual(curry(sum)(1, 2)(3), 6)
+    assert.strictEqual(curry(sum)(1)(2)(3), 6)
+    assert.strictEqual(curry(sum)(1)(2, 3), 6)
+  })
+})
