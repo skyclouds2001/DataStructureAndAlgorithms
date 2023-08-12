5ea3943eb8af0bfe47cb8688b0e8bb6f4759113a
skyclouds2001 <skyclouds2001@163.com>
2023-08-12 16:24:44 +0800

    feat: done 1670

--- LeetCode/1670.cpp
+++ LeetCode/1670.cpp
@@ -6,30 +6,36 @@ class FrontMiddleBackQueue {
 private:
     deque<int> frontQueue;
     deque<int> backQueue;
+    int size;
 public:
-    FrontMiddleBackQueue() {}
+    FrontMiddleBackQueue() {
+        this->size = 0;
+    }
 
     void pushFront(int val) {
         this->frontQueue.push_front(val);
+        ++this->size;
     }
 
     void pushMiddle(int val) {
-        while (this->frontQueue.size() - this->backQueue.size() > 0) {
+        while (!this->frontQueue.empty() && this->frontQueue.size() - this->backQueue.size() > 0) {
             int val = this->frontQueue.back();
             this->frontQueue.pop_back();
             this->backQueue.push_front(val);
         }
-        while (this->backQueue.size() - this->frontQueue.size() > 1) {
+        while (!this->backQueue.empty() && this->backQueue.size() - this->frontQueue.size() > 1) {
             int val = this->backQueue.front();
             this->backQueue.pop_front();
             this->frontQueue.push_back(val);
         }
 
         this->frontQueue.push_back(val);
+        ++this->size;
     }
 
     void pushBack(int val) {
         this->backQueue.push_back(val);
+        ++this->size;
     }
 
     int popFront() {
@@ -38,10 +44,12 @@ public:
         } else if (this->frontQueue.empty()) {
             int val = this->backQueue.front();
             this->backQueue.pop_front();
+            --this->size;
             return val;
         } else {
             int val = this->frontQueue.front();
             this->frontQueue.pop_front();
+            --this->size;
             return val;
         }
     }
@@ -51,12 +59,12 @@ public:
             return -1;
         }
 
-        while (this->frontQueue.size() - this->backQueue.size() > 1) {
+        while (!this->frontQueue.empty() && this->frontQueue.size() - this->backQueue.size() > 0) {
             int val = this->frontQueue.back();
             this->frontQueue.pop_back();
             this->backQueue.push_front(val);
         }
-        while (this->backQueue.size() - this->frontQueue.size() > 1) {
+        while (!this->backQueue.empty() && this->backQueue.size() - this->frontQueue.size() > 1) {
             int val = this->backQueue.front();
             this->backQueue.pop_front();
             this->frontQueue.push_back(val);
@@ -65,17 +73,19 @@ public:
         if (this->frontQueue.size() == this->backQueue.size() + 1) {
             int val = this->frontQueue.back();
             this->frontQueue.pop_back();
+            --this->size;
             return val;
         } else if (this->frontQueue.size() + 1 == this->backQueue.size()) {
             int val = this->backQueue.front();
             this->backQueue.pop_front();
+            --this->size;
             return val;
         } else {
             int val = this->frontQueue.back();
             this->frontQueue.pop_back();
+            --this->size;
             return val;
         }
-
     }
 
     int popBack() {
@@ -84,10 +94,12 @@ public:
         } else if (this->backQueue.empty()) {
             int val = this->frontQueue.back();
             this->frontQueue.pop_back();
+            --this->size;
             return val;
         } else {
             int val = this->backQueue.back();
             this->backQueue.pop_back();
+            --this->size;
             return val;
         }
     }
