b0a353902190ecbc3d8828cae18ccf030014b6b5
skyclouds2001 <skyclouds2001@163.com>
2023-08-15 17:35:03 +0800

    feat: update 49

--- LeetCode/49.cpp
+++ LeetCode/49.cpp
@@ -1,38 +1,25 @@
+#include "vector"
+#include "string"
+#include "map"
+#include "algorithm"
+#include "unordered_map"
+
+using namespace std;
+
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<string, vector<string>> m;
-        for (auto str: strs) {
-            string hash = transformStringToHash(str);
-
-            if (m.count(hash) == 0) {
-                vector<string> v;
-                v.push_back(str);
-                m.insert(pair<string, vector<string>>(hash, v));
-            } else {
-                m.at(hash).push_back(str);
-            }
+        unordered_map<string, vector<string>> m;
+        for (auto& str: strs) {
+            string hash = str;
+            sort(hash.begin(), hash.end());
+            m[hash].emplace_back(str);
         }
 
         vector<vector<string>> ans;
         for (auto an: m) {
-            ans.push_back(an.second);
+            ans.emplace_back(an.second);
         }
         return ans;
-    }
-
-    string transformStringToHash(string s) {
-        int sum[26] = {0};
-
-        for (auto c: s) {
-            ++sum[c - 'a'];
-        }
-
-        string res;
-        for (auto s: sum) {
-            res.append(" " + to_string(s));
-        }
-
-        return res;
     }
 };
