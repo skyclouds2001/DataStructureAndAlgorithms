37039289f0e86fb1883f6fc4274ae2d02c13f986
skyclouds2001 <skyclouds2001@163.com>
2023-08-17 12:41:09 +0800

    feat: done 383

---
+++ LeetCode/383.cpp
@@ -0,0 +1,27 @@
+#include "string"
+
+using namespace std;
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int count[26] = {0};
+
+        if (ransomNote.length() > magazine.length()) {
+            return false;
+        }
+
+        for (auto c: magazine) {
+            ++count[c - 'a'];
+        }
+
+        for (auto c: ransomNote) {
+            --count[c - 'a'];
+            if (count[c - 'a'] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
