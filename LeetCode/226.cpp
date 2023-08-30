cbef42f74dfbecafd6dfd99c597f423faa85de4f
skyclouds2001 <skyclouds2001@163.com>
2023-08-30 19:29:03 +0800

    feat: update 226

--- LeetCode/226.cpp
+++ LeetCode/226.cpp
@@ -12,3 +12,30 @@ public:
         return root;
     }
 };
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr) {
+            return root;
+        }
+
+        stack<TreeNode*> stack;
+
+        stack.push(root);
+        while(!stack.empty()) {
+            TreeNode* node = stack.top();
+            stack.pop();
+
+            swap(node -> left, node -> right);
+            if (node -> left != nullptr) {
+                stack.push(node -> left);
+            }
+            if (node -> right != nullptr) {
+                stack.push(node -> right);
+            }
+        }
+
+        return root;
+    }
+};
