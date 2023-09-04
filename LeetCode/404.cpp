01cd8790d89c6a5057700e54bcf719e3a1112d2c
skyclouds2001 <skyclouds2001@163.com>
2023-09-05 00:14:58 +0800

    feat: update 404

--- LeetCode/404.cpp
+++ LeetCode/404.cpp
@@ -16,3 +16,37 @@ public:
         return ls + rs;
     }
 };
+
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        queue<TreeNode*> q;
+        int sum = 0;
+
+        if (root != nullptr) {
+            q.push(root);
+        }
+
+        while (!q.empty()) {
+            const int size = q.size();
+
+            for (int i = 0; i < size; ++i) {
+                auto p = q.front();
+                q.pop();
+
+                if (p -> left != nullptr && p -> left -> left == nullptr && p -> left -> right == nullptr) {
+                    sum += p -> left -> val;
+                }
+
+                if (p -> left != nullptr) {
+                    q.push(p -> left);
+                }
+                if (p -> right != nullptr) {
+                    q.push(p -> right);
+                }
+            }
+        }
+
+        return sum;
+    }
+};
