ec3209855df8f1f95725c22ea74f72dbc7607ea7
skyclouds2001 <skyclouds2001@163.com>
2023-09-10 12:18:45 +0800

    feat: done 530

---
+++ LeetCode/530.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    vector<int> v;
+    void traversal(TreeNode* node) {
+        if (node == nullptr) {
+            return;
+        }
+
+        traversal(node -> left);
+        v.push_back(node -> val);
+        traversal(node -> right);
+    }
+public:
+    int getMinimumDifference(TreeNode* root) {
+        traversal(root);
+
+        const int size = v.size();
+        int ans = INT_MAX;
+        for (int i = 1; i < size; ++i) {
+            ans = min(ans, v[i] - v[i - 1]);
+        }
+
+        return ans;
+    }
+};
