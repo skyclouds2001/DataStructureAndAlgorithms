6d9b358a234c78d12764a38fb0d3a8d810a8376f
skyclouds2001 <skyclouds2001@163.com>
2023-09-11 21:42:52 +0800

    feat: done 108

---
+++ LeetCode/108.cpp
@@ -0,0 +1,19 @@
+class Solution {
+private:
+    TreeNode* traversal(vector<int>& nums, int left, int right) {
+        if (left > right) {
+            return nullptr;
+        }
+
+        int index = (left + right) / 2;
+
+        TreeNode* l = traversal(nums, left, index - 1);
+        TreeNode* r = traversal(nums, index + 1, right);
+
+        return new TreeNode(nums[index], l, r);
+    }
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return traversal(nums, 0, nums.size() - 1);
+    }
+};
