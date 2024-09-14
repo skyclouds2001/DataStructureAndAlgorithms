79bf2e829efb62dfc7c954813d1dfa9c96a342af
skyclouds2001 <skyclouds2001@163.com>
2024-09-15 00:14:15 +0800

    feat: update template

--- template.cpp
+++ template.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
 
 int main(int argc, char const *argv[]) {
     string str;
